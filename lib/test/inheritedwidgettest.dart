a55c08356011d02cf35b31213cc6b19cc752c8b6
hongtao liu <hongtao2.liu@dhc.com.cn>
2022-04-18 08:07:05 +0800

    test

--- lib/test/inheritedwidgettest.dart
+++ lib/test/inheritedwidgettest.dart
@@ -125,30 +125,37 @@ class ProviderRoute extends StatefulWidget {
 class _ProviderRouteState extends State<ProviderRoute> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: ChangeNotifierProvider<CartModel>(
-        data: CartModel(),
-        child: Builder(builder: (context) {
-          return Column(
-            children: <Widget>[
-              Builder(builder: (context){
-                var cart=ChangeNotifierProvider.of<CartModel>(context);
-                return Text("总价: ${cart.totalPrice}");
-              }),
-              Builder(builder: (context){
-                print("ElevatedButton build"); //在后面优化部分会用到
-                return ElevatedButton(
-                  child: Text("添加商品"),
-                  onPressed: () {
-                    //给购物车中添加商品，添加后总价会更新
-                    ChangeNotifierProvider.of<CartModel>(context).add(Item(20.0, 1));
-                  },
+    return Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: ChangeNotifierProvider<CartModel>(
+              data: CartModel(),
+              child: Builder(builder: (context) {
+                return Column(
+                  children: <Widget>[
+                    Builder(builder: (context){
+                      var cart=ChangeNotifierProvider.of<CartModel>(context);
+                      return Text("总价: ${cart.totalPrice}");
+                    }),
+                    Builder(builder: (context){
+                      print("ElevatedButton build"); //在后面优化部分会用到
+                      return ElevatedButton(
+                        child: Text("添加商品"),
+                        onPressed: () {
+                          //给购物车中添加商品，添加后总价会更新
+                          ChangeNotifierProvider.of<CartModel>(context).add(Item(20.0, 1));
+                        },
+                      );
+                    }),
+                  ],
                 );
               }),
-            ],
-          );
-        }),
-      ),
+            ),
+          ),
+
+          Text("heaventest"),
+        ]
     );
   }
 }
